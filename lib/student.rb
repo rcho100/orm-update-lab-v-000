c8578d543b3290f848796e94f8a070ad0fc1fd15
Richard Cho <richardcho100@gmail.com>
2018-10-25 03:08:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,9 @@ class Student
     sql = <<-SQL
       SELECT * FROM students WHERE name = ?
     SQL
-    DB[:conn].execute(sql, name)
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end
   end
 
 end
